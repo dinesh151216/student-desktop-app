549b851d014e9945547e43495cb0817d6bc7fb6a
Dinesh Dhankhar <dineshdhankhar76626@gmail.com>
2026-02-05 11:20:56 +0530

    Stage 1: Create window and database

--- app.py
+++ app.py
@@ -1,1 +1,30 @@
-print("Student App Started")
+import sqlite3
+import tkinter as tk
+
+# ---------------- DATABASE SETUP ----------------
+
+def create_database():
+    conn = sqlite3.connect("students.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS students(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            age INTEGER,
+            address TEXT
+        )
+    """)
+
+    conn.commit()
+    conn.close()
+
+# ---------------- UI SETUP ----------------
+
+root = tk.Tk()
+root.title("Student Management System")
+root.geometry("400x300")
+
+create_database()
+
+root.mainloop()
